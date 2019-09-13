112f56425d1126c1fa155a5affcf631570bac438
assadk88 <assadkhan@outlook.com>
2019-09-13 21:43:53 +0100

    Add chalk dependency

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var chalk = require('chalk');
+// you shoudl leave a new line between your requires
+// and the rest of the program
 
 var app = express();
 
+// executes a function for any requests
+// to the root route
 app.get('/', function(req, res){
   res.send("Hello, world!")
 });
 
+// defines the port on which express is listening on
 port = 4040
 app.listen(port, function(){
-  console.log("listening on port " + port)
+  console.log(`listening on port ${chalk.green(port)}`)
 });
