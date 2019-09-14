b26aa9e90916e4ffbf2f8809a45a60873ce8dcd8
assadk88 <assadkhan@outlook.com>
2019-09-15 00:25:40 +0100

    Clarified comments

--- app.js
+++ app.js
@@ -3,29 +3,24 @@ var chalk   = require('chalk');
 var debug   = require('debug')('app');
 var morgan  = require('morgan');
 var path    = require('path')
-// you shoudl leave a new line between your requires
-// and the rest of the program
 
 var app = express();
 
 app.use(morgan('tiny'));
 
-//Static Filepaths
-
+//Set static filepaths
 app.use(express.static(path.join(__dirname, '/public'))); // sets up static directory for static files
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 
-// executes a function for any requests
-// to the root route
-// serves up the static 'index.html' page
+// Serves the static 'index.html' page for the '/' route
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-// defines the port on which express is listening on
+// Defines the listening port
 port = 4040
 app.listen(port, function(){
   debug(`listening on port ${chalk.green(port)}`)
