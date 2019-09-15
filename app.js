0bba1998400374e2a02ac911cbe26a4f34443485
assadk88 <assadkhan@outlook.com>
2019-09-15 19:28:13 +0100

    Minor cleanup of code

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 var express = require('express');
-var chalk   = require('chalk');
-var debug   = require('debug')('app');
-var morgan  = require('morgan');
-var path    = require('path')
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
 app.use(morgan('tiny'));
 
-//Set static filepaths
+// Set static filepaths
+
 app.use(express.static(path.join(__dirname, '/public'))); // sets up static directory for static files
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 
 // Serves the static 'index.html' page for the '/' route
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 // Defines the listening port
-port = 4040
-app.listen(port, function(){
-  debug(`listening on port ${chalk.green(port)}`)
+var port = 4040;
+app.listen(port, function () {
+  debug(`listening on port ${chalk.green(port)}`);
 });
